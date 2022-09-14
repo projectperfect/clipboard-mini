015857c9d315fd28f8d0151d9778474d660340f8
projectperfect <jprfct@protonmail.com>
2022-09-14 00:26:18 -0500

    hero section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ module.exports = {
     extend: {
       colors: {
         strongCyan: 'hsl(171, 66%, 44%)',
-        lightBlue: 'hsl(233, 100%, 69%',
-        darkGrayishBlue: 'hsl(210, 10%, 33%',
-        grayishBlue: 'hsl(201, 11%, 66%',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
       },
       fontFamily: {
         sans: ['Bai Jamjuree', 'sans-serif'],
